16a94e741552d69b1fee1abd0aea1fa767f3e69d
parth020806 <parthchugh750@gmail.com>
2023-07-28 16:48:53 +0530

    cart

--- Project001/urls.py
+++ Project001/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('users/forgetpassword/', forget_password),
     path('contact/', contact_view, name='contact'),
     path('product_details/<str:product_id>/', product_details, name='product_details'),
-    path('cart/username', views.cart_view, name='cart'),
+    path('cart/username', cart_view, name='cart'),
 ]
